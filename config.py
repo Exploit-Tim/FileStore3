365c24145264fcd2acda51d88333e8c4b5177d92
ᴛɪᴍᴇ ɪs ᴍᴏɴᴇʏ <codexyraa@gmail.com>
2025-06-04 00:21:47 +0700

    Update config.py

--- config.py
+++ config.py
@@ -18,22 +18,22 @@ from logging.handlers import RotatingFileHandler
 #rohit_1888 on Tg
 #--------------------------------------------
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "8154426339")
-APP_ID = int(os.environ.get("APP_ID", "")) #Your API ID from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "") #Your API Hash from my.telegram.org
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7887341780")
+APP_ID = int(os.environ.get("APP_ID", "26550183")) #Your API ID from my.telegram.org
+API_HASH = os.environ.get("API_HASH", "25be30608ef422623c7d8a55bfb98c62") #Your API Hash from my.telegram.org
 #--------------------------------------------
 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-10021708388")) #Your db channel Id
-OWNER = os.environ.get("OWNER", "Zerozerozoro") # Owner username without @
-OWNER_ID = int(os.environ.get("OWNER_ID", "73229001")) # Owner id
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002535004474")) #Your db channel Id
+OWNER = os.environ.get("OWNER", "astaboynich") # Owner username without @
+OWNER_ID = int(os.environ.get("OWNER_ID", "6904648429")) # Owner id
 #--------------------------------------------
 PORT = os.environ.get("PORT", "8001")
 #--------------------------------------------
-DB_URI = os.environ.get("DATABASE_URL", "")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://wtfbruh:KontolXD#123@fsub.brzgete.mongodb.net/?retryWrites=true&w=majority&appName=fsub")
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluooo")
 #--------------------------------------------
 FSUB_LINK_EXPIRY = int(os.getenv("FSUB_LINK_EXPIRY", "150"))  # 0 means no expiry
-BAN_SUPPORT = os.environ.get("BAN_SUPPORT", "https://t.me/Zerozerozoro")
+BAN_SUPPORT = os.environ.get("BAN_SUPPORT", "https://t.me/AstaSuportt")
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "200"))
 #--------------------------------------------
 START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/ec17880d61180d3312d6a.jpg")
